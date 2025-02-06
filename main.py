56d3212b963f8db6838f8c736311974a44d714f1
Jayesh-Ingale <ingalejayesh21@gmail.com>
2025-02-06 15:53:17 -0500

    data ingestion pipeline added

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import pandas
+from IntelImageClassification import logger
+from IntelImageClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
